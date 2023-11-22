e98e56f18f26b826815c90fba20eacf8b8a4cf85
SWAPNIL00007 <115202938+SWAPNIL00007@users.noreply.github.com>
2023-11-22 16:38:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+//below synatx is used to import listem item compnent to this filr app.js
+import ListItem from "./components/ListItem";
+// In jsx the tags are written in first letter should be captial this is know as pascal cas notion
+const App = () =>{
+return(
+<div>
+  <h1>hello-world</h1>
+  <ListItem></ListItem>
+</div>
+); 
+}
+export default App;
---
+++ ListItem.js
@@ -0,0 +1,11 @@
+//how small compents is made given below and how we can make compents in react is  given below:
+//sytanx given below
+const ListItem = () => {
+    return(
+    <div>
+    <img src="\assets\index-of-img.png" alt="Some Title"></img>
+    </div>
+    )
+}
+//export default list item is used so that we can export this listitem file any other file
+export default ListItem
